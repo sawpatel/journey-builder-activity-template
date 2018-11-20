6b07386b10bf07ec678400688907848b977fb140
sawpatel <sawan.patel@safelite.com>
2018-11-20 13:51:25 -0500

    Automate API Call

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.raw({type: 'application/jwt'}));
 //app.use(express.methodOverride());
 //app.use(express.favicon());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.env.dirPath, 'public')));
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
