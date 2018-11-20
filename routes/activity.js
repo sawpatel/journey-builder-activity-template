e8c9f62fd8dfef4dc20ddd90920520a37e0a279e
sawpatel <sawan.patel@safelite.com>
2018-11-20 14:25:28 -0500

    Dir name fix

--- routes/activity.js
+++ routes/activity.js
@@ -3,7 +3,7 @@ var util = require('util');
 
 // Deps
 //const Path = require('path');
-const JWT = require(__dirname + '/lib/jwtDecoder.js'));
+const JWT = require('/lib/jwtDecoder.js'));
 var util = require('util');
 var http = require('https');
 var request = require('request');
